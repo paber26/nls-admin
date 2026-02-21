31bc7033c7f0132af067c3787ef771da1d09eb6a
Bernaldo Napitupulu <bernaldonapitupulu@Bernaldos-MacBook-Pro.local>
2026-02-21 11:14:49 +0800

    Coba prod

--- src/main.js
+++ src/main.js
@@ -2,16 +2,8 @@ import { createApp } from "vue"
 import "./style.css"
 import App from "./App.vue"
 import router from "./router"
-// import { CKEditor } from "@ckeditor/ckeditor5-vue"
-// import { CkeditorPlugin } from "@ckeditor/ckeditor5-vue"
-// import { CkeditorPlugin } from "@ckeditor/ckeditor5-vue"
-// import "ckeditor5/ckeditor5.css" // CSS sekarang jadi satu
-import { CkeditorPlugin } from "@ckeditor/ckeditor5-vue" // Sesuai Gambar 1
+import { CkeditorPlugin } from "@ckeditor/ckeditor5-vue"
 import "ckeditor5/ckeditor5.css"
-// import CKEditor from "@ckeditor/ckeditor5-vue"
-
-// import * as CKTest from "@ckeditor/ckeditor5-vue"
-// console.log("Isi CKEditor Package:", CKTest)
 
 const app = createApp(App)
 app.use(router)
